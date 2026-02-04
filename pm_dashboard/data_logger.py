8bd3b091e11359b28aa4e0e95be96069c15829b9
Cavon <cavonxx@gmail.com>
2026-02-04 17:18:12 +0800

    refactor(data_logger): remove deprecated code and simplify data handling
    - Remove deprecated system monitoring functions and PWM fan control
    - Remove SPC peripheral support to simplify the class
    - Improve data handling by adding JSON serialization for lists
    - Clean up unused methods and simplify the data flow

--- pm_dashboard/data_logger.py
+++ pm_dashboard/data_logger.py
@@ -6,27 +6,12 @@ from influxdb import InfluxDBClient
 
 from .utils import log_error
 
-# deprecated
-from sf_rpi_status import \
-    get_cpu_temperature, \
-    get_gpu_temperature, \
-    get_cpu_percent, \
-    get_cpu_freq, \
-    get_cpu_count, \
-    get_memory_info, \
-    get_disk_info, \
-    get_disks_info, \
-    get_boot_time, \
-    get_ips, \
-    get_macs, \
-    get_network_connection_type, \
-    get_network_speed, \
-    PWMFan
+import json
 
 class DataLogger:
 
     @log_error
-    def __init__(self, database=None, interval=1, spc_enabled=False, log=None):
+    def __init__(self, database=None, interval=1, log=None):
         self.log = log or logging.getLogger(app_name)
         self._is_ready = False
 
@@ -36,30 +21,14 @@ class DataLogger:
             self.log.error(f"Failed to connect to influxdb: {e}")
             return
 
-        self.spc = None
-
         self.thread = None
         self.running = False
 
         self.db = database
         self.interval = interval
-        if spc_enabled:
-            self.log.info("SPC peripheral enabled")
-            from spc.spc import SPC
-            self.spc = SPC()
-        else:
-            self.log.debug("SPC peripheral disabled")
         
-        # deprecated
-        self.pwm_fan = PWMFan()
-
         self.status = {}
         self.__read_data__ = None
-
-    # deprecated
-    @log_error
-    def update_status(self, status):
-        self.status = status
 
     @log_error
     def set_read_data(self, func):
@@ -70,80 +39,11 @@ class DataLogger:
         self.interval = interval
 
     @log_error
-    def _get_data(self): # deprecated
-        boot_time = get_boot_time()
-        ips = get_ips()
-        macs = get_macs()
-        network_connection_type = get_network_connection_type()
-        network_speed = get_network_speed()
-
-        data = {}
-        data['cpu_temperature'] = float(get_cpu_temperature()) if get_cpu_temperature() is not None else None
-        data['gpu_temperature'] = float(get_gpu_temperature()) if get_gpu_temperature() is not None else None
-        data['cpu_percent'] = float(get_cpu_percent())
-        data['cpu_count'] = int(get_cpu_count())
-
-        if  self.pwm_fan._is_ready:
-            data['pwm_fan_speed'] = self.pwm_fan.get_speed()
-
-        cpu_freq = get_cpu_freq()
-        data['cpu_freq'] = float(cpu_freq.current)
-        data['cpu_freq_min'] = float(cpu_freq.min)
-        data['cpu_freq_max'] = float(cpu_freq.max)
-
-        cpu_percents = get_cpu_percent(percpu=True)
-        for i, percent in enumerate(cpu_percents):
-            data[f'cpu_{i}_percent'] = float(percent)
-
-        memory = get_memory_info()
-        data['memory_total'] = int(memory.total)
-        data['memory_available'] = int(memory.available)
-        data['memory_percent'] = float(memory.percent)
-        data['memory_used'] = int(memory.used)
-
-        disks = get_disks_info()
-        for disk_name in disks:
-            disk = disks[disk_name]
-            data[f'disk_{disk_name}_mounted'] = int(disk.mounted)
-            data[f'disk_{disk_name}_total'] = int(disk.total)
-            data[f'disk_{disk_name}_used'] = int(disk.used)
-            data[f'disk_{disk_name}_free'] = int(disk.free)
-            data[f'disk_{disk_name}_percent'] = float(disk.percent)
-
-        data['boot_time'] = float(boot_time)
-
-        ips = get_ips()
-        for name in ips:
-            data[f'ip_{name}'] = ips[name]
-
-        macs = get_macs()
-        for name in macs:
-            data[f'mac_{name}'] = macs[name]
-
-        data['network_type'] = "&".join(network_connection_type)
-        data['network_upload_speed'] = int(network_speed.upload)
-        data['network_download_speed'] = int(network_speed.download)
-
-        for name in self.status:
-            data[name] = self.status[name]
-
-        if self.spc is not None:
-            spc = self.spc.read_all()
-            for key in spc:
-                data[key] = spc[key]
-
-        for key in data:
-            value = data[key]
-            if isinstance(value, bool):
-                data[key] = int(value)
-        return data
-
-    @log_error
     def get_data(self):
-        if self.__read_data__ is not None:
-            data = self.__read_data__()
-        else:
-            data = self.get_data() # deprecated
+        if self.__read_data__ is None:
+            self.log.error("No read data function set")
+            return {}
+        data = self.__read_data__()
         if data == {}:
             return {}
 
@@ -152,7 +52,7 @@ class DataLogger:
             if isinstance(value, bool):
                 value = int(value)
             elif isinstance(value, list):
-                continue
+                value = json.dumps(value)
             elif isinstance(value, dict):
                 continue
             new_data[key] = value
@@ -168,8 +68,6 @@ class DataLogger:
                     status, msg = self.db.set('history', data)
                     if not status:
                         self.log.error(f"Failed to set data: {msg}")
-                # else:
-                    # self.log.debug(f"Set data: {new_data}")
 
             elapsed = time.time() - start
             if elapsed < self.interval:
